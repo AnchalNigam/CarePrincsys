e710061c48791f436d0df84abda100dcc50d06aa
Anchal Nigam <32920850+AnchalNigam@users.noreply.github.com>
2017-12-25 21:50:24 -0800

    Add files via upload

--- careprincsys.sql
+++ careprincsys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2017 at 03:29 AM
+-- Generation Time: Dec 26, 2017 at 08:17 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -37,7 +37,7 @@ CREATE TABLE `user` (
   `address` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `state` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
-  `contact` int(15) NOT NULL,
+  `contact` bigint(11) NOT NULL,
   `occupation` varchar(255) NOT NULL,
   `type` int(11) NOT NULL,
   `domain` text NOT NULL
@@ -48,8 +48,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `gender`, `address`, `state`, `city`, `contact`, `occupation`, `type`, `domain`) VALUES
-(4, 'Anchal Nigam', 'anchalnigamm@gmail.com', '643b33d20fbe52d48538f4cd7b24f593', 'F', '2/6,kareli', 'Uttar Pradesh', 'Allahabad', 2147483647, 'Web Developer', 1, ''),
-(6, 'Sakshi Nigam', 'saknigam12@gmail.com', 'b73a3203047396075ccac51f92358f6e', 'F', '2/5,colnelganj', 'Uttar Pradesh', 'Allahabad', 2147483647, 'Web Developer', 4, 'Education');
+(6, 'Sakshi Nigam', 'saknigam12@gmail.com', 'b73a3203047396075ccac51f92358f6e', 'F', '2/5,colnelganj', 'Uttar Pradesh', 'Allahabad', 2147483647, 'Web Developer', 4, 'Education'),
+(8, 'Anchal Nigam', 'anchalnigamm@gmail.com', '643b33d20fbe52d48538f4cd7b24f593', 'F', '2/6,kareli', 'Uttar Pradesh', 'Allahabad', 9026224948, 'Web Developer', 1, '');
 
 -- --------------------------------------------------------
 
@@ -65,6 +65,15 @@ CREATE TABLE `userlog` (
   `logout` varchar(255) NOT NULL,
   `status` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `useremail`, `userip`, `logintime`, `logout`, `status`) VALUES
+(1, 'anchalnigamm@gmail.com', 0x3a3a3100000000000000000000000000, '2017-12-26 18:14:46', '26-12-2017 11:56:39 PM', 1),
+(2, 'anchalnigamm@gmail.com', 0x3a3a3100000000000000000000000000, '2017-12-26 18:28:44', '', 0),
+(3, 'anchalnigamm@gmail.com', 0x3a3a3100000000000000000000000000, '2017-12-26 18:29:03', '27-12-2017 12:00:26AM', 1);
 
 --
 -- Indexes for dumped tables
@@ -90,13 +99,13 @@ ALTER TABLE `userlog`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `userlog`
 --
 ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
