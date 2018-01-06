9ef103c45a9075b018b7bd19e0f008dbcb91af37
AnchalNigam <anchalnigamm@gmail.com>
2018-01-05 20:59:58 -0800

    done with admin signup and login

---
+++ admin/resumes.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Princsys Admin Panel - Personal Details</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+        <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="admin.html" class="simple-text">
+                    Admin
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="personaldetails.php">
+                        <i class="pe-7s-graph"></i>
+                        <p>Personal Details</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="resumes.php">
+                        <i class="pe-7s-user"></i>
+                        <p>Resume</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-panel">
+        <!--<nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="personaldetails.php" style="margin-right: 600px">Resumes</a>
+                </div>
+
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                    <ul class="nav navbar-nav navbar-right" >
+                        <li>
+                           <a href="#">
+                               Account
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Log out
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+         <div class="content">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="header">
+                        <h4 class="title">Resumes</h4>
+                        <p class="category"> The list of resumes</p>
+                    </div>
+                    <div class="content">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h5>Resumes:</h5>
+
+                                <?php
+
+    // $connection = new mysqli('localhost', 'root', 'root', 'validationtestintern');
+
+        // $error_flag = 0;
+    // $result;
+    // if ($connection->connect_error) {
+        // die('connection failed: '.$connection->connect_error);
+    // }
+    // else
+    // {
+
+    // $sql ="SELECT * FROM interndetails";
+    // $result= mysqli_query($connection,$sql);
+    // if(mysqli_num_rows($result) > 0 ) {
+
+        // echo'<table class="table table-striped text-center">
+                                                // <tr>
+                                                    // <th>Name</th>
+                                                    // <th>E-mail</th>
+                                                    // <th>Resumes</th>
+                                                // </tr>';
+            
+    // while ($row = mysqli_fetch_array($result)) {
+    // $path = "resumes/";
+    // $link = "<a href='".$path.$row['resume']."'>".$row['resume'];
+   // header("Content-disposition: inline; filename=".basename('$path/'.$row['resume'].'pdf'));
+    // echo    '<tr>
+                // <td>'.$row['name'].'</td>
+                // <td>'.$row['email'].'</td>
+                // <td>'."$link".$row['resume'].'</td>
+            // </tr>';
+
+
+            // }
+        // echo '</table>';
+        // }
+    // }
+?>
+
+                            </div class="col-md-6">
+                        </div class="row">
+                    </div class="content">            
+                            
+</body>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <!--  Checkbox, Radio & Switch Plugins -->
+    <script src="assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+    <!--  Charts Plugin -->
+    <script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+    <script src="assets/js/light-bootstrap-dashboard.js"></script>
+
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <script src="assets/js/demo.js"></script>
+
+</html>
---
+++ admin/sign.php
@@ -0,0 +1,200 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+include('include/config.php');
+$error="";
+$success="";
+if(isset($_POST['submit']))
+{
+	$Name=$_POST['name'];
+	
+	$password=$_POST['password'];
+	$answer=$_POST['answer'];
+	
+		
+	
+    
+	$contact=$_POST['mob_no'];
+	if($password==$_POST['con_password']) {
+	if($answer=="helpothers") {
+	      
+
+		$password=md5($_POST['password']);
+		$query=mysqli_query($bd,"insert into admin(username,password,contactno) values('$Name','$password','$contact')");
+		if($query)
+{
+   
+	$success="You are successfully Become a part of Care Princsys";
+
+
+}
+else{
+    $error="Not registered,something went worng";
+
+}
+	}
+	else{
+		$error="Security Answer does not match!!";
+		
+	}
+	}
+	else{
+		$error="Password and Confirm Password does not match!!";
+	}
+	
+	
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Princsys Admin Panel</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+ <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+<link type="text/css" href="assets/css/theme.css" rel="stylesheet">
+ <link type="text/css" href="assets/css/font-awesome.css" rel="stylesheet">
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+     <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+</head>
+<body>
+
+<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				
+
+			  	<a class="nav navbar-brand" href="index.php">
+			  		Care Princsys
+			  	</a>
+
+			
+				
+					<ul class="nav pull-right">
+
+						<li><a href="index.php">
+						Login
+						
+						</a></li>
+
+						
+
+						
+					</ul>
+			
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
+
+     
+ 
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="module module-login span4 offset4">
+				<?php if(!empty($error)){
+				echo "<div class='alert alert-danger fade in'>
+				<a href='#' class='close' data-dismiss='alert'>&times;</a>
+				<strong>$error</strong> </div>"; 
+				} ?>
+					<?php if(!empty($success)){
+				echo "<div class='alert alert-success fade in'>
+				<a href='#' class='close' data-dismiss='alert'>&times;</a>
+				<strong>$success</strong> </div>"; 
+				} ?>
+					<form class="form-vertical" method="post">
+						<div class="module-head">
+							<h3>Sign Up</h3>
+						</div>
+						<span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+						<div class="module-body">
+							<div class="control-group">
+								<div class="controls row-fluid">
+								 <h5>Personal Information</h5>
+									
+									<input class="span12" type="text"  name="mob_no" placeholder="Contact Number"pattern="[0-9]+" minlength="10"
+				maxlength="10" class="mycls" title="Please only enter digits" required><br/><br/>
+									<input class="span12" type="text"  name="answer" placeholder="CarePrincsys Security Answer" pattern="[A-Za-z ]+" maxlength="65" title="Hindustane Answer" required><br/><br/>
+									<br/><h5>Login Information</h5>
+									<input class="span12" type="text" name="name" placeholder="User Name" pattern="[A-Za-z ]+" maxlength="65" title="Username should only contain letters. e.g. john" required><br/><br/>
+									<input class="span12" type="text" name="password" placeholder="Password" minlength="8" maxlength="25"
+					title="Password should be between 8 - 25 letters" required><br/><br/>
+									<input class="span12" type="text" name="con_password" placeholder="Confirm Password" required><br/><br/>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls row-fluid">
+						<input class="span12 btn btn-primary" type="submit" name="submit">
+								</div>
+							</div>
+						</div>
+						<div class="module-foot">
+							<div class="control-group">
+								<div class="controls clearfix">
+									
+									
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div><!--/.wrapper-->
+<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2018 Care Princsys </b> All rights reserved.
+		</div>
+	</div>
+	<!--   Core JS Files   -->
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+   
+ <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--  Checkbox, Radio & Switch Plugins -->
+    <script src="assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+    <!--  Charts Plugin -->
+    <script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    
+
+   
+
+    <!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+    <script src="assets/js/demo.js"></script>
+
+</html>
+
+</body>  
+  
+  
+  
